db9668a469c3804134e39ecf4c3396c2ab69a1a6
Leonard van Avesaath <leonard@hoek-steen.nl>
2026-06-03 21:25:53 +0200

    The first one

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,11 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
+import { Welcome } from '../welcome/welcome';
+import { Example } from '../components/sidemenu/sidemenu';
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+	return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }];
 }
 
 export default function Home() {
-  return <Welcome />;
+	return <Example />;
 }
